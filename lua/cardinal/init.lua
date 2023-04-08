b53a51dd21886422c0d0e232d6568162ae7298e1
Blake Lawyer <blakethelawyer@gmail.com>
2023-04-08 11:23:59 -0500

    floating window test

--- lua/cardinal/init.lua
+++ lua/cardinal/init.lua
@@ -1,5 +1,32 @@
+local function create_floating_window()
+    local buf = vim.api.nvim_create_buf(false, true)
+    local width = vim.api.nvim_get_option("columns")
+    local height = vim.api.nvim_get_option("lines")
+
+    local win_height = math.ceil(height * 0.8 - 4)
+    local win_width = math.ceil(width * 0.8)
+    local row = math.ceil((height - win_height) / 2 - 1)
+    local col = math.ceil((width - win_width) / 2)
+
+    local opts = {
+        style = "minimal",
+        relative = "editor",
+        width = win_width,
+        height = win_height,
+        row = row,
+        col = col
+    }
+
+    local win = vim.api.nvim_open_win(buf, true, opts)
+
+    return buf, win
+end
+
 local function Cardinal()
-    print("Hello from Cardinal!")
+    local buf, win = create_floating_window()
+
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, {"Hello from the floating window!"})
+    vim.api.nvim_win_set_option(win, "wrap", false)
 end
 
 vim.api.nvim_command('command! Cardinal lua require("cardinal").Cardinal()')
