014ec626533ab25c95b7e6070ae14eb28b11ea1b
Blake Lawyer <blakethelawyer@gmail.com>
2023-04-08 11:19:31 -0500

    first :Function

--- lua/cardinal/init.lua
+++ lua/cardinal/init.lua
@@ -1,6 +1,8 @@
 local function Cardinal()
-    print("Hello from function")
+    print("Hello from Cardinal!")
 end
+
+vim.api.nvim_command('command! Cardinal lua require("cardinal").Cardinal()')
 
 return {
     Cardinal = Cardinal
