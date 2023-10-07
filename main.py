b9a6df60a0854e6d10d3eb25cc8afdd1a0392fb8
Blake Lawyer <blakethelawyer@gmail.com>
2023-10-07 06:16:46 +0000

    learning styling and widget selection via query

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from textual import on
 from textual.app import App, ComposeResult
 from textual.reactive import var
 from textual.screen import Screen
-from textual.widgets import Footer, Header, Label, ListItem, ListView, Static
+from textual.widgets import Footer, Header, Label, ListItem, ListView, Static, Button
+from textual.containers import Container, Center
 import json
 
 with open('cards.txt', 'r') as file:
@@ -16,16 +17,23 @@ class Study(Screen):
 
         yield Header()
 
-        yield Label("ASDFASDFASDF", classes="border") 
+        yield Center(
+            Label("CARD FRONT", shrink=True, id="card-content"),
+            Button("Default", id="button"),
+            Button("Default", id="button2"),
+        id="card-container")
 
         yield ListView(
             ListItem(Label("Back"), id="menu"),
         )
+
+
         yield Footer()
 
     def on_mount(self) -> None:
         self.title = "Cardinal"
         self.sub_title = "Study"
+        self.set_focus(self.query_one("#button2"))
 
 class Create(Screen):
 
@@ -56,9 +64,7 @@ class Edit(Screen):
 
 
 class Cardinal(App):
-
     CSS_PATH = "styles.tcss"
-
 
     def compose(self) -> ComposeResult:
 
