692c6ab9fa77c183c1ecc9dc1ab64f0308847a9a
Blake Lawyer <blakethelawyer@gmail.com>
2024-05-01 23:06:04 -0500

    card information and cloze extracted

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,8 @@ use std::process;
 
 fn load_database() -> Result<(), rusqlite::Error> {
 
-    println!("{} connecting to cardinal database", "load_database:".green());
     let conn = Connection::open("cardinal.db")?;
 
-    println!("{} creating cards table", "load_database:".green());
     conn.execute(
         "CREATE TABLE IF NOT EXISTS cards (
             id INTEGER PRIMARY KEY,
@@ -35,7 +33,6 @@ fn main() -> io::Result<()> {
     
     let card_divider = "=".repeat(terminal_width);
 
-    println!("{} loading database", "main:".green());
     let _ = load_database();
 
     // Open cards file for parsing.
@@ -48,27 +45,19 @@ fn main() -> io::Result<()> {
     let mut card_count: i32 = 0;
     let mut inside_card: bool = false;
     let mut card_information = String::new();
-
+    let mut cards = Vec::new();  
+    
     for line in reader.lines() {
         let line = line?;
 
-        // what are the possible states when parsing?
-        // not inside card, detect start
-        // inside card
-        // inside card, detect end
-
         if line.starts_with("\"") && line.chars().nth(1).is_some() && !inside_card {
 
-            println!("{} start of card detected", "main:".blue());
-            
             inside_card = true;
             card_information.clear();
             card_information += &line; 
             
-        } else if line == "\"" {
+        } else if line == "\"[" {
 
-            println!("{} end of card detected", "main:".blue());
-            
             inside_card = false;
             card_count += 1;
             
@@ -84,21 +73,41 @@ fn main() -> io::Result<()> {
                 process::exit(0);
             }
             
-            println!("{}", card_information.blue());
-
             // Remove inactive clozes.
             let re = Regex::new(r#"<span class=""cloze-inactive"" data-ordinal=""."">(.*?)<\/span>"#).unwrap(); 
             card_information = re.replace_all(&card_information, "$1").to_string();
 
-            // Remove <br> at the end.
-            let re = Regex::new(r#"<br>"#).unwrap(); 
+            // Remove <br><br>.
+            let re = Regex::new(r"<br><br>").unwrap();
+            card_information = re.replace_all(&card_information, " ").to_string();
+
+            // Remove <br> at the end of cards.
+            let re = Regex::new(r"<br>").unwrap();
             card_information = re.replace_all(&card_information, "$1").to_string();
 
-            println!("{}", card_information.white());
-            println!("{}", card_divider.red());
+            // Extract the card cloze.
+            let re = Regex::new(r#"<span class=""cloze"" data-ordinal=""."">(.*?)<\/span>"#).unwrap(); 
+
+            let mut card_cloze = String::new();
+            
+            for cap in re.captures_iter(&card_information) {
+                card_cloze.push_str(&cap[1]);  
+            }
+
+            card_information = re.replace_all(&card_information, "[...]").to_string();
+
+            // Add the card information and cloze to our vector.
+            cards.push((card_information.clone(), card_cloze.clone()));                       
+
+            // Print the card front and back.
+            println!("{}", "Card Front:".blue());
+            println!("{}\n", card_information.white());
+
             card_information.clear();
 
-            // insert into db / print card info logic
+            println!("{}", "Card Back:".blue());
+            println!("{}", card_cloze.white());
+            println!("{}", card_divider.red());
 
         } else {
             card_information += &line; 
